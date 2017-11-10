b7573e4f1f28e5f84ca2056cd5f8c1bc4529ec92
CSharpFri <CSharpFri@CLASS-2-02>
2017-11-10 19:14:38 +0100

    Add project files.

---
+++ obrazce/Ctverec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obrazce
+{
+    class Ctverec : Obrazec
+    {
+        double strana;
+        public Ctverec(double strana)
+        {
+            this.strana = strana;
+        }
+        public override double VypoctiObsah()
+        {
+            return (strana * strana);
+        }
+    }
+}
